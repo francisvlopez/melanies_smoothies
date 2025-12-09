b38d75bf625bb8eec6a83eb041e1207c32c9fce8
francisvlopez <francis.v.lopez@ibm.com>
2025-12-09 22:10:42 +0800

    Fetch and display nutrition info for selected fruits
    Added functionality to fetch and display nutrition information for selected fruits.

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,8 @@ import streamlit as st
 
 from snowflake.snowpark.functions import col
 
+import requests
+
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
@@ -45,7 +47,12 @@ if ingredients_list:
     session.sql(my_insert_stmt).collect()
     st.success(f'Your Smoothie is ordered, {name_of_order}!', icon="✅")
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+if ingredients_list:
+  ingredients_string = ''
+
+  for fruit_chosen in ingredients_list:
+    ingredients_string += fruit_chosen + ' '
+    st.subheader(fruit_chosen + ' Nutrition Information')
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon" + fruit_chosen)
+    #st.text(smoothiefroot_response.json())
+    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
